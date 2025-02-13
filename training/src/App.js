b581f16fc102fc529a15e7b6378fcb5228fdd327
jeevaneshatechgroup <jeevan@eshatechgroup.com>
2025-02-13 17:32:26 +0530

    upadte

---
+++ training/src/App.js
@@ -0,0 +1,14 @@
+import React from'react'
+import Demo from '../src/component/Demo'
+import Button from '../src/component/button'
+
+function App() {
+  return (
+    <div >
+      <Demo/>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
---
+++ training/src/component/button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyButton() {
+  return (
+    <button onClick={() => alert('Button clicked!')}>
+      Click Me
+    </button>
+  );
+}
+
+export default MyButton;
